b6b15cadd3c1cf97da94b8c22465d6f750a89eb2
sagarkh1997 <sagar.khuteta@spit.ac.in>
2022-03-03 15:46:05 -0500

    Implemented listClients method

--- mohamma9/src/common_methods.h
+++ mohamma9/src/common_methods.h
@@ -9,41 +9,52 @@
 #include <sys/select.h>
 #include <arpa/inet.h>
 
-
-void getIP(void) {
+void getIP(void)
+{
 
     int _socket;
-    struct sockaddr_in _address,my_addr;
+    struct sockaddr_in _address, my_addr;
     char myIP[16];
     socklen_t sin_size;
     _address.sin_family = AF_INET;
     _address.sin_port = htons(53);
     _address.sin_addr.s_addr = inet_addr("8.8.8.8");
 
-    if ((_socket = socket(AF_INET,SOCK_DGRAM,0)) == -1) {
+    if ((_socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+    {
         printf("UDP socket error");
     }
 
-    bind(_socket,( struct sockaddr*)&_address, sizeof(_address));
+    bind(_socket, (struct sockaddr *)&_address, sizeof(_address));
 
-    connect(_socket, ( struct sockaddr*)&_address, sizeof(_address));
+    connect(_socket, (struct sockaddr *)&_address, sizeof(_address));
     sin_size = sizeof my_addr;
     bzero(&my_addr, sizeof(my_addr));
-    getsockname(_socket, ( struct sockaddr*)&_address,&sin_size);
+    getsockname(_socket, (struct sockaddr *)&_address, &sin_size);
 
     inet_ntop(AF_INET, &_address.sin_addr, myIP, sizeof(myIP));
-    printf("IP %s\n",myIP);
+    printf("IP %s\n", myIP);
 
-    close(_socket); 
+    close(_socket);
 }
 
-void getPort(struct sockaddr_in *socket_addr) {
+void getPort(struct sockaddr_in *socket_addr)
+{
     int myPort;
     myPort = ntohs(socket_addr->sin_port);
     printf("Local port : %u\n", myPort);
 }
 
-void getAuthor(void) {
+void getAuthor(void)
+{
     ("I, %s, have read and understood the course academic integrity policy.\n", "mohamma9");
 }
-void listClients(struct sockaddr_in* socket_list[]);
+void listClients(struct sockaddr_in *socket_list[])
+{
+    int size_socket_list = sizeof(*socket_list);
+    for (int i = 0; i < size_socket_list; i++)
+    {
+        printf("Internet Address of client %d is %d \n ", i, socket_list[i]->sin_addr);
+        printf("Port Address of client %d is %d \n ", i, socket_list[i]->sin_port);
+    }
+}
--- mohamma9/src/server.c
+++ mohamma9/src/server.c
@@ -8,12 +8,12 @@
 // #include <sys/time.h>
 #include <sys/select.h>
 
-int main() {
-
+int main()
+{
     int server_socket, total_clients = 30, client_connections[30], client_socket, max_socket_descriptors,
-    addrlen, sd, valread;
+                       addrlen, sd, valread;
     fd_set read_descriptors;
-    char buf[1000],s[20];
+    char buf[1000], s[20];
     struct sockaddr_in server_address;
     struct sockaddr_storage client_address;
 
@@ -21,66 +21,68 @@ int main() {
     server_address.sin_port = htons(9002);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
-    for (int i = 0; i < total_clients; i++)  
-    {  
-        client_connections[i] = 0;  
+    for (int i = 0; i < total_clients; i++)
+    {
+        client_connections[i] = 0;
     }
 
-    if((server_socket = socket(AF_INET,SOCK_STREAM,0)) == -1){
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
         printf("Server: Error");
         exit(EXIT_FAILURE);
     }
 
-    bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+    bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address));
     listen(server_socket, 5);
 
-    addrlen = sizeof(server_address);  
+    addrlen = sizeof(server_address);
     puts("Waiting for connections ...");
 
-    while(1) {
-        FD_ZERO(&read_descriptors);  
-     
-        //add master socket to set 
-        FD_SET(server_socket, &read_descriptors); 
-        FD_SET(STDIN_FILENO, &read_descriptors); 
-        max_socket_descriptors = server_socket; 
+    while (1)
+    {
+        FD_ZERO(&read_descriptors);
 
-        for ( int i = 0 ; i < total_clients ; i++)  
-        {  
-            //socket descriptor 
-            sd = client_connections[i];  
-                 
-            //if valid socket descriptor then add to read list 
-            if(sd > 0)  
-                FD_SET( sd , &read_descriptors);  
-                 
-            //highest file descriptor number, need it for the select function 
-            if(sd > max_socket_descriptors)  
-                max_socket_descriptors = sd;  
+        // add master socket to set
+        FD_SET(server_socket, &read_descriptors);
+        FD_SET(STDIN_FILENO, &read_descriptors);
+        max_socket_descriptors = server_socket;
+
+        for (int i = 0; i < total_clients; i++)
+        {
+            // socket descriptor
+            sd = client_connections[i];
+
+            // if valid socket descriptor then add to read list
+            if (sd > 0)
+                FD_SET(sd, &read_descriptors);
+
+            // highest file descriptor number, need it for the select function
+            if (sd > max_socket_descriptors)
+                max_socket_descriptors = sd;
         }
-
 
         select(max_socket_descriptors + 1, &read_descriptors, NULL, NULL, NULL);
 
-        if (FD_ISSET(server_socket, &read_descriptors)) {
+        if (FD_ISSET(server_socket, &read_descriptors))
+        {
 
-            if ((client_socket = accept(server_socket, 
-                    (struct sockaddr *)&client_address, (socklen_t*)&addrlen))<0)  
-            {  
-                perror("accept");  
-                exit(EXIT_FAILURE);  
-            } 
+            if ((client_socket = accept(server_socket,
+                                        (struct sockaddr *)&client_address, (socklen_t *)&addrlen)) < 0)
+            {
+                perror("accept");
+                exit(EXIT_FAILURE);
+            }
 
-            for (int i = 0; i < total_clients; i++)  
-            {  
-                //if position is empty 
-                if( client_connections[i] == 0 )  
-                {  
-                    client_connections[i] = client_socket;  
-                    printf("Adding to list of sockets as %d\n" , i);  
-                         
-                    break;  
-                }  
+            for (int i = 0; i < total_clients; i++)
+            {
+                // if position is empty
+                if (client_connections[i] == 0)
+                {
+                    client_connections[i] = client_socket;
+                    printf("Adding to list of sockets as %d\n", i);
+
+                    break;
+                }
             }
             // char buf[1024];
             // memset(buf, 0, sizeof(buf));
@@ -96,43 +98,44 @@ int main() {
             //     close(server_socket);
             // }
         }
-        else if (FD_ISSET(STDIN_FILENO, &read_descriptors)) {
+        else if (FD_ISSET(STDIN_FILENO, &read_descriptors))
+        {
             char msg[1024];
             memset(msg, 0, sizeof(msg));
             read(STDIN_FILENO, msg, sizeof(msg));
             printf(msg);
         }
-        for (int i = 0; i < total_clients; i++)  
-        {  
-            sd = client_connections[i];  
-                
-            if (FD_ISSET( sd , &read_descriptors))  
-            {  
-                //Check if it was for closing , and also read the 
-                //incoming message 
-                if ((valread = read( sd , buf, 1024)) == 0)  
-                {  
-                    //Somebody disconnected , get his details and print 
-                    getpeername(sd , (struct sockaddr*)&client_address , \
-                        (socklen_t*)&addrlen);  
-                    // printf("Host disconnected , ip %s , port %d \n" , 
-                    //     inet_ntoa(client_address.sin_addr) , ntohs(((struct sockaddr* )client_address).sin_port));  
-                        
-                    //Close the socket and mark as 0 in list for reuse 
-                    close( sd );  
-                    client_connections[i] = 0;  
-                }  
-                    
-                //Echo back the message that came in 
-                else 
-                {  
-                    //set the string terminating NULL byte on the end 
-                    //of the data read 
-                    buf[valread] = '\0';  
+        for (int i = 0; i < total_clients; i++)
+        {
+            sd = client_connections[i];
+
+            if (FD_ISSET(sd, &read_descriptors))
+            {
+                // Check if it was for closing , and also read the
+                // incoming message
+                if ((valread = read(sd, buf, 1024)) == 0)
+                {
+                    // Somebody disconnected , get his details and print
+                    getpeername(sd, (struct sockaddr *)&client_address,
+                                (socklen_t *)&addrlen);
+                    // printf("Host disconnected , ip %s , port %d \n" ,
+                    //     inet_ntoa(client_address.sin_addr) , ntohs(((struct sockaddr* )client_address).sin_port));
+
+                    // Close the socket and mark as 0 in list for reuse
+                    close(sd);
+                    client_connections[i] = 0;
+                }
+
+                // Echo back the message that came in
+                else
+                {
+                    // set the string terminating NULL byte on the end
+                    // of the data read
+                    buf[valread] = '\0';
                     // send(sd , buf , strlen(buf) , 0 );
-                    printf(buf);  
-                }  
-            }  
-        }  
+                    printf(buf);
+                }
+            }
+        }
     }
 }
